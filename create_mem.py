af81956e4179893e8b954a7be4ff2f57d9b5199d
alestrab <astrabello@estudante.ufscar.br>
2023-02-07 16:22:39 -0300

    Ex 1

---
+++ create_mem.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+data = np.loadtxt('ram.txt',dtype=str)
+new_data = []
+for i in range(0,len(data)):
+    new_data.append('RAM['+str(i)+'] <= 32\'h' + data[i] + ';')
+
+filename='ram-hex.txt'
+np.savetxt(filename,new_data,fmt='%s')
